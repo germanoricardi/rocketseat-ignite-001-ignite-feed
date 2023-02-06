3eb9e5670d9d0b1f6cea2145aa35cad3dbe69254
Germano Ricardi <germanoricardi7@gmail.com>
2023-02-06 06:06:17 -0400

    Implementado estrutura html dos comentários;

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return(
+        <div className={styles.comment}>
+            <img src="https://github.com/germanoricardi.png" alt="Germano Ricardi" />
+
+            <div className={styles.commentBox}>
+                
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Germano Ricardi</strong>
+                            <time title="25 de Janeiro de 2023 às 6:27h" dateTime='2023-01-25 06:27:30'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp /> Aplaudir <span>20</span>
+                    </button>
+                </footer>
+
+            </div>
+
+        </div>
+    )
+}
