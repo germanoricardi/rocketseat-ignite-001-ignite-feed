f0fab3af5c26c6bf4a61f92c3ea6d239f3a61f27
Germano Ricardi <germanoricardi7@gmail.com>
2023-02-09 21:26:17 -0400

    Alterado componente Post para receber dados dinâmicos;

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,37 @@ import './global.css'
 
 function App() {
 
+  const posts = [
+    {
+      id: 1,
+      author: {
+        avatarUrl: 'https://github.com/germanoricardi.png',
+        name: 'Germano Ricardi',
+        role: 'Desenvolvedor Full Stack'
+      },
+      content: [
+        { type: 'paragraph', content: 'Fala galera 👋' },
+        { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+        { type: 'link', content: 'jane.design/doctorcare' },
+      ],
+      publishedAt: new Date('2023-02-09 20:48:00'),
+    },
+    {
+      id: 2,
+      author: {
+        avatarUrl: 'https://github.com/maykbrito.png',
+        name: 'Mayk Brito',
+        role: 'Educator @Rocketseat'
+      },
+      content: [
+        { type: 'paragraph', content: 'Fala galera 👋' },
+        { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+        { type: 'link', content: 'jane.design/doctorcare' },
+      ],
+      publishedAt: new Date('2023-02-09 08:48:00'),
+    },
+  ]
+
   return (
     <div>
       <Header />
@@ -15,9 +46,18 @@ function App() {
         <Sidebar />
         
         <main>
-          <Post />
-          
-          <Post />
+
+          {
+            posts.map(post => {
+              return (
+                <Post
+                  author      = {post.author}
+                  content     = {post.content}
+                  publishedAt = {post.publishedAt}
+                />
+              )
+            })
+          }
         </main>
       </div>
     </div>
